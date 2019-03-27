05d0bf31d2beca1e2787102d777d3556ddd4b165
hidollara <hidollara@users.noreply.github.com>
2019-03-28 01:01:41 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 DOTPATH="${HOME}/.dotfiles"
-if [ ! -e ${DOTPATH} ]; then git clone git@github.com:hidollara/dotfiles.git ~/.dotfiles; fi
+if [ ! -e "${DOTPATH}" ]; then git clone git@github.com:hidollara/dotfiles.git "${DOTPATH}"; fi
 cd ~/.dotfiles
 make prepare
 make deploy
