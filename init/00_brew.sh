8cd7df8a0ac7e1e087fba0e78f5345ee66520223
hidollara <hidollara@users.noreply.github.com>
2019-05-08 12:50:15 +0900

    install brew on install.sh

--- init/00_brew.sh
+++ init/00_brew.sh
@@ -4,9 +4,6 @@ install_if_not_installed() {
   if ! brew ls --versions "$1" >/dev/null; then brew install "$1"; fi
 }
 
-if ! command -v brew >/dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
 install_if_not_installed zsh
 install_if_not_installed tmux
 install_if_not_installed vim
--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
-DOTPATH="${HOME}/.dotfiles"
-if [ ! -e "$DOTPATH" ]; then git clone https://github.com/hidollara/dotfiles.git "$DOTPATH"; fi
+DOTPATH="$HOME/.dotfiles"
+
+if ! command -v brew >/dev/null; then
+  case "$OSTYPE" in
+    darwin* )
+      /usr/bin/ruby -e "$(curl -fsSL https://raw/githubusercontent.com/Homebrew/install/master/install)"
+      ;;
+    linux* )
+      if command -v apt >/dev/null; then
+        sudo apt -y install build-essential curl file git
+      elif command -v dnf >/dev/null; then
+        sudo dnf -y groupinstall 'Development Tools' && sudo dnf -y install curl file git
+      else
+        exit 1
+      fi
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+      export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+      ;;
+    * )
+      exit 1
+      ;;
+  esac
+fi
+test -e "$DOTPATH" || git clone https://github.com/hidollara/dotfiles.git "$DOTPATH"
 cd "$DOTPATH"
 make init
 make deploy
